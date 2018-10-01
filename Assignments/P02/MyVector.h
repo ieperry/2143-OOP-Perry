d52ed1e8fdaf16aecce002709ab342fe51016b0a
Isha Perry <toufgirl99@aol.com>
2018-10-01 10:39:36 -0500

    Update MyVector.h

--- Assignments/P02/MyVector.h
+++ Assignments/P02/MyVector.h
@@ -7,45 +7,60 @@
 
 #include <iostream>
 
+/*
+*Initializes the class for MyVector, and
+*lists all of it's methods
+*
+*Isha Perry
+*/
+
+#include <iostream>
+
 using namespace std;
 
 class MyVector
 {
 private:
-	int *vPointer;
-	int vSize;
-	int index; 
+int *vPointer;
+int vSize;
+int index; 
 public:
 
   //Default constructor for class
   MyVector();
 
   //Parameterized contructor for class
-	MyVector(int arrSize);
+  MyVector(int arrSize);
 
   //Adds item to back of vector array
-	void pushBack(int item);
+  void pushBack(int item);
 
+  //Adds the items from any array to the back
+  //of the vector array
   void pushBack(int *arr, int arrSize); 
 
   //Removes item from the back of vector array
-	int popBack(int arrSize);
+  int popBack(int arrSize);
 
   //Prints the vector
 	void print();
 
   //Returns the size of the vector array
-	int size();
+  int size();
 
   //Returns the index of the vector array 
   int vIndex();  
 
   //Finds how full the vector array is with 
   //a percentage
-	double percentFull();
+  double percentFull();
 
   //Allows the vector to act like an array
   int& operator[](int i); 
+
+  //Sets the values of two vector arrays to 
+  //equal each other
+  void operator=(const MyVector& V); 
 
   //Allows two vectors to be added together
   MyVector operator+(MyVector& V2);   
@@ -58,11 +73,6 @@ public:
 
   //Allows two vectors to be divided together
   MyVector operator/(MyVector& V2); 
-
-  //Works seperate from the addition operator
-  //to the division operater and does work
-  //when setting two vectors to equal one another
-  //MyVector operator=(const MyVector& V);  
 
   //Tests the equality of two vectors
   bool operator==(MyVector& V2); 
